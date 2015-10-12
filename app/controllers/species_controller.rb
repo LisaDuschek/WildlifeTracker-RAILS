fdf643e7b5af9f3071f7849d34307c9383d5fbe8
Andrew Brennwald & Lisa Duschek <student@epicodus.com>
2015-10-12 15:45:04 -0700

    Implement updating of species details.

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -27,13 +27,19 @@ class SpeciesController < ApplicationController
     render :show
   end
 
-  # def edit
-  #
-  # end
+  def edit
+    @species = Species.find(params[:id])
+    render :edit
+  end
 
-  # def update
-  #
-  # end
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(species_params)
+      redirect_to species_index_path
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #
