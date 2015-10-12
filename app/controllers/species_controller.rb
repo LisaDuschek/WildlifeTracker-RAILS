4dcd5bbb4daa6ba4c9af9d53846bfbbdfd2136b2
Andrew Brennwald & Lisa Duschek <student@epicodus.com>
2015-10-12 15:40:20 -0700

    Implement adding new species.

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,16 +1,26 @@
 class SpeciesController < ApplicationController
   def index
-    @species = Species.all
+    @all_species = Species.all
     render :index
   end
 
-  # def create
-  #
-  # end
+  def create
+    @all_species = Species.all
+    @species = Species.new(species_params)
+    if @species.save
+      redirect_to species_index_path
+    else
+      render :new
+    end
+  end
 
-  # def new
-  #
-  # end
+
+
+  def new
+    @species = Species.new
+    render :new
+
+  end
 
   def show
     @species = Species.find(params[:id])
@@ -28,5 +38,9 @@ class SpeciesController < ApplicationController
   # def destroy
   #
   # end
+  private
+  def species_params
+   params.require(:species).permit(:name)
+  end
 
 end
